35cd86dabd31cc762fed0590c3c977340a1fbacd
Comotellame07 <157026631+Comotellame07@users.noreply.github.com>
2024-01-18 20:57:30 +0100

    Update dnsclient.sh

--- dnsclient.sh
+++ dnsclient.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo "Configurando Netplan..."
-sed -i "s/dhcp4: yes/dhcp4: no/g" /etc/netplan/00-installer-config.yaml
+sed -i "s/dhcp4: yes/dhcp4: no/g" /etc/netplan/01-network-manager-all.yaml
 nic=`ifconfig | awk 'NR==1{print $1}'`
 read -p "IP Estática Ej. 192.168.100.10/24: " staticip 
 read -p "IP router: " gatewayip
 read -p "Servidores DNS: " nameserversip
 read -p "Dominio: " dominio
 TTL='$TTL'
-cat > /etc/netplan/00-installer-config.yaml <<EOF
+cat > /etc/netplan/01-network-manager-all.yaml <<EOF
 network:
   version: 2
   ethernets:
