227070bb0d9895529f12aaa8ebf98690d9852e97
Indiana630 <125204578+Indiana630@users.noreply.github.com>
2024-01-18 16:16:25 +0100

    Update netplan.sh

--- netplan.sh
+++ netplan.sh
@@ -3,7 +3,7 @@
 # Creates a backup
 cp /etc/netplan/00-installer-config.yaml /etc/netplan/00-installer-config.yaml.bk_`date +%Y%m%d%H%M`
 # Changes dhcp from 'yes' to 'no'
-sed -i "s/dhcp4: yes/dhcp4: no/g" /etc/00-installer-config.yaml
+sed -i "s/dhcp4: yes/dhcp4: no/g" /etc/netplan/00-installer-config.yaml
 # Retrieves the NIC information
 nic=`ifconfig | awk 'NR==1{print $1}'`
 # Ask for input on network configuration
