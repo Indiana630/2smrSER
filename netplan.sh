527b807ccafa2bc929b407bc185fac370d030811
Comotellame07 <dariusblack0706@gmail.com>
2024-01-18 14:33:01 +0100

    netplan

--- netplan.sh
+++ netplan.sh
@@ -7,21 +7,22 @@ sed -i "s/dhcp4: yes/dhcp4: no/g" /etc/netplan/01-netcfg.yaml
 # Retrieves the NIC information
 nic=`ifconfig | awk 'NR==1{print $1}'`
 # Ask for input on network configuration
-read -p "Enter the static IP of the server in CIDR notation: " staticip 
-read -p "Enter the IP of your gateway: " gatewayip
-read -p "Enter the IP of preferred nameservers (seperated by a coma if more than one): " nameserversip
+read -p "IP Estática: " staticip 
+read -p "IP router: " gatewayip
+read -p "Servidores DNS: " nameserversip
+read -p "Dominio: "
 echo
 cat > /etc/netplan/01-netcfg.yaml <<EOF
 network:
   version: 2
-  renderer: networkd
   ethernets:
     $nic
       addresses:
-        - $staticip
+      - $staticip
       gateway4: $gatewayip
       nameservers:
-          addresses: [$nameserversip]
+       addresses: [$nameserversip]
+       search: [$dominio]
 EOF
 sudo netplan apply
 echo "==========================="
