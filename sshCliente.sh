6c327995aee94e5df01ab72f325c76728724fc4c
Indiana630 <125204578+Indiana630@users.noreply.github.com>
2024-01-18 17:50:17 +0100

    Update sshCliente.sh

--- sshCliente.sh
+++ sshCliente.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-read -p "Antes de comenzar, recuerda tener creada la carpeta .ssh en el servidor, sin esta el script no funcionara (Presiona enter para continuar) " si
 read -p "IP servidor: " ipserver
 ssh-keygen -t rsa
 scp /home/usuario/.ssh/id_rsa.pub usuario@$ipserver:/home/usuario/
