2090d1c2a1f4429e4cc425e9f3d6b3ad2b900c99
Indiana630 <125204578+Indiana630@users.noreply.github.com>
2024-01-18 17:27:23 +0100

    Update sshCliente.sh

--- sshCliente.sh
+++ sshCliente.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 read -p "IP servidor: " ipserver
 ssh-keygen -t rsa
-scp /home/usuario/.ssh/authorized_keys usuario@$ipserver:/home/usuario/.ssh
+scp /home/usuario/.ssh/id_rsa.pub usuario@$ipserver:/home/usuario/.ssh
 scp /home/usuario/.ssh/sshServidor.sh usuario@$ipserver:/home/usuario/
 echo "Archivo de autorizaciones creado correctamente, porfavor continue en el servidor..."
 echo "Conectando con el servidor..."
