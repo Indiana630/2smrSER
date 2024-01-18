643c343bef11a79ec22824fd623d043afaa61a02
Indiana630 <125204578+Indiana630@users.noreply.github.com>
2024-01-18 17:44:44 +0100

    Update sshCliente.sh

--- sshCliente.sh
+++ sshCliente.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+read -p "Antes de comenzar, recuerda tener creada la carpeta .ssh en el servidor, sin esta el script no funcionara (Presiona enter para continuar) " si
 read -p "IP servidor: " ipserver
 ssh-keygen -t rsa
 scp /home/usuario/.ssh/id_rsa.pub usuario@$ipserver:/home/usuario/.ssh
-scp /home/usuario/.ssh/sshServidor.sh usuario@$ipserver:/home/usuario/
+scp sshServidor.ssh usuario@$ipserver:/home/usuario/
 echo "Archivo de autorizaciones creado correctamente, porfavor continue en el servidor..."
 echo "Conectando con el servidor..."
 ssh usuario@$ipserver
