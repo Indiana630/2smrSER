d1b51a7e07252c23938548e1b13097fa1595e5df
Comotellame07 <157026631+Comotellame07@users.noreply.github.com>
2024-01-18 17:08:22 +0100

    Update dns.sh

--- dns.sh
+++ dns.sh
@@ -1,1 +1,33 @@
-#!bin/bash
+#!/bin/bash
+sed -i "s/dhcp4: yes/dhcp4: no/g" /etc/netplan/00-installer-config.yaml
+nic=`ifconfig | awk 'NR==1{print $1}'`
+read -p "IP Estática: " staticip 
+read -p "IP router: " gatewayip
+read -p "Servidores DNS: " nameserversip
+read -p "Dominio: " dominio
+echo
+cat > /etc/netplan/00-installer-config.yaml <<EOF
+network:
+  version: 2
+  ethernets:
+    $nic
+      dhcp4: false
+      addresses:
+      - $staticip
+      gateway4: $gatewayip
+      nameservers:
+       addresses: [$nameserversip]
+       search: [$dominio]
+EOF
+netplan apply
+cat > /etc/systemd/resolved.conf <<EOF
+[Resolve]
+DNS=$staticip
+Domains=$dominio
+EOF
+systemctl daemon-reload
+systemctl restart systemd-networkd
+systemctl restart systemd-resolved
+rm -f /etc/resolv.conf
+ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
+echo
