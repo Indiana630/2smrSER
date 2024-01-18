fabef512060c685831a710fa4bcb1f4612a99a8f
Indiana630 <125204578+Indiana630@users.noreply.github.com>
2024-01-18 16:11:29 +0100

    Update netplan.sh

--- netplan.sh
+++ netplan.sh
@@ -3,14 +3,14 @@
 # Creates a backup
 cp /etc/netplan/00-installer-config.yaml /etc/netplan/00-installer-config.yaml.bk_`date +%Y%m%d%H%M`
 # Changes dhcp from 'yes' to 'no'
-sed -i "s/dhcp4: yes/dhcp4: no/g" /etc/netplan/01-netcfg.yaml
+sed -i "s/dhcp4: yes/dhcp4: no/g" /etc/00-installer-config.yaml
 # Retrieves the NIC information
 nic=`ifconfig | awk 'NR==1{print $1}'`
 # Ask for input on network configuration
 read -p "IP Estática: " staticip 
 read -p "IP router: " gatewayip
 read -p "Servidores DNS: " nameserversip
-read -p "Dominio: "
+read -p "Dominio: " dominio
 echo
 cat > /etc/netplan/01-netcfg.yaml <<EOF
 network:
