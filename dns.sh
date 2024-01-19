1f4b6ff0666b6d68d367432eac46f82fddfabfef
Comotellame07 <157026631+Comotellame07@users.noreply.github.com>
2024-01-19 12:45:28 +0100

    Update dns.sh

--- dns.sh
+++ dns.sh
@@ -102,6 +102,62 @@ read -p "¿Deseas aplicar los cambios?(y/n): " respuesta
 aplicarcambios
 }
 
+staticip() {
+read -p "IP Estática Ej. 192.168.100.10/24: " staticip
+read -p "¿Estas seguro?(y/n)" resp
+if [ $resp = "y" ]
+then
+echo "OK"
+elif [ $resp = "n" ]
+then
+staticip
+else
+staticip
+fi
+}
+
+gatewayip() {
+read -p "IP router: " gatewayip
+read -p "¿Estas seguro?(y/n)" resp
+if [ $resp = "y" ]
+then
+echo "OK"
+elif [ $resp = "n" ]
+then
+gatewayip
+else
+gatewayip
+fi
+}
+
+nameserversip() {
+read -p "Servidores DNS: " nameserversip
+read -p "¿Estas seguro?(y/n)" resp
+if [ $resp = "y" ]
+then
+echo "OK"
+elif [ $resp = "n" ]
+then
+nameserversip
+else
+nameserversip
+fi
+}
+
+dominio() {
+read -p "Dominio: " dominio
+read -p "¿Estas seguro?(y/n)" resp
+if [ $resp = "y" ]
+then
+echo "OK"
+elif [ $resp = "n" ]
+then
+dominio
+else
+dominio
+fi
+}
+
 echo "Configurando Netplan..."
 sed -i "s/dhcp4: yes/dhcp4: no/g" /etc/netplan/00-installer-config.yaml
 nic=`ifconfig | awk 'NR==1{print $1}'`
@@ -125,7 +181,10 @@ network:
        search: [$dominio]
 EOF
 netplan apply
-echo "Netplan configurado correctamente"
+echo "Netplan operativo"
+sleep 2
+clear
+echo "Configurando resolv.conf"
 cat > /etc/systemd/resolved.conf <<EOF
 [Resolve]
 DNS=$staticip
@@ -137,6 +196,9 @@ systemctl restart systemd-resolved
 rm -f /etc/resolv.conf
 ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
 echo "Resolvconf operativo"
+sleep 2
+clear
+echo "Configurando nsswitch.conf"
 cat > /etc/nsswitch.conf <<EOF
 passwd:         files systemd
 group:          files systemd
@@ -153,7 +215,10 @@ rpc:            db files
 
 netgroup:       nis
 EOF
-echo "Nsswitch configurado correctamente"
+echo "Nsswitch operativo"
+sleep 2
+clear
+echo "Configurando zonas"
 cat > /etc/bind/named.conf.local <<EOF
 zone "$dominio" {
         type master;
@@ -165,7 +230,7 @@ zone "$inversa.in-addr.arpa" {
         file "/etc/bind/db.$inversa";
 };
 EOF
-echo "Zonas configuradas correctamente"
+echo "Zonas operativas"
 cp /etc/bind/db.local /etc/bind/db.default
 cat > /etc/bind/db.default <<EOF
 ;
