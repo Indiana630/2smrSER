01ca99d4bfe374bb4a593fe899f805be9001e92c
Comotellame07 <157026631+Comotellame07@users.noreply.github.com>
2024-01-25 13:48:59 +0100

    Update dhcp.sh

--- rs/dhcp.sh
+++ rs/dhcp.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+menu() {
+echo "1. Instalar servicio dhcp"
+echo "2. Configurar adactador de red (netplan)"
+echo "3. Configurar servicio dhcp"
+read -p "Escoje una opccion" menuresp
+if [ menuresp = 1 ]
+apt install isc-dhcp-server
+}
