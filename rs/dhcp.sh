2a3b10c84326a86d1b076ca1a6aa38d318d8f9fe
Comotellame07 <157026631+Comotellame07@users.noreply.github.com>
2024-01-25 14:14:36 +0100

    Update dhcp.sh

--- rs/dhcp.sh
+++ rs/dhcp.sh
@@ -12,6 +12,89 @@ clear
 echo "Servicio instalado con exito"
 menu
 elif [ menuresp = 2 ]
+  if [ adaptadores = 1 ]
+  then
+  
 then
 
 }
+
+netplan1() {
+staticip
+gatewayip
+nameserversip
+dominio
+echo
+cat > /etc/netplan/00-installer-config.yaml <<EOF
+network:
+  version: 2
+  ethernets:
+    $nic
+      dhcp4: false
+      addresses:
+      - $staticip
+      gateway4: $gatewayip
+      nameservers:
+       addresses: [$nameserversip]
+       search: [$dominio]
+EOF
+sudo netplan apply
+echo "Netplan configurado correctamente"
+}
+
+staticip() {
+read -p "IP Estática Ej. 192.168.100.10/24: " staticip
+read -p "¿Estas seguro?(y/n)" resp
+if [ $resp = "y" ]
+then
+echo "OK"
+elif [ $resp = "n" ]
+then
+staticip
+else
+staticip
+fi
+}
+
+gatewayip() {
+read -p "IP router: " gatewayip
+read -p "¿Estas seguro?(y/n)" resp
+if [ $resp = "y" ]
+then
+echo "OK"
+elif [ $resp = "n" ]
+then
+gatewayip
+else
+gatewayip
+fi
+}
+
+nameserversip() {
+read -p "Servidores DNS: " nameserversip
+read -p "¿Estas seguro?(y/n)" resp
+if [ $resp = "y" ]
+then
+echo "OK"
+elif [ $resp = "n" ]
+then
+nameserversip
+else
+nameserversip
+fi
+}
+
+dominio() {
+read -p "Dominio: " dominio
+read -p "¿Estas seguro?(y/n)" resp
+if [ $resp = "y" ]
+then
+echo "OK"
+elif [ $resp = "n" ]
+then
+dominio
+else
+dominio
+fi
+}
+
