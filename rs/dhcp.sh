b22b53c24ca2aad3ffb4e226fb84b2b3763094b0
Comotellame07 <157026631+Comotellame07@users.noreply.github.com>
2024-01-25 14:12:16 +0100

    Update dhcp.sh

--- rs/dhcp.sh
+++ rs/dhcp.sh
@@ -5,5 +5,13 @@ echo "2. Configurar adactador de red (netplan)"
 echo "3. Configurar servicio dhcp"
 read -p "Escoje una opccion" menuresp
 if [ menuresp = 1 ]
+then
+apt update isc-dhcp-server
 apt install isc-dhcp-server
+clear
+echo "Servicio instalado con exito"
+menu
+elif [ menuresp = 2 ]
+then
+
 }
